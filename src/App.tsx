d1b659883fe9cd7b933cf44fc6fac5f973611415
Sam Daived <sam.daived@tieto.com>
2026-06-28 22:01:51 +0300

    before-deply

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./contexts/AuthContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
@@ -26,7 +26,7 @@ const App = () => (
           <AuthProvider>
             <Toaster />
             <Sonner />
-            <HashRouter>
+            <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/login" element={<Login />} />
@@ -66,7 +66,7 @@ const App = () => (
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
-            </HashRouter>
+            </BrowserRouter>
           </AuthProvider>
         </LanguageProvider>
       </ThemeProvider>
